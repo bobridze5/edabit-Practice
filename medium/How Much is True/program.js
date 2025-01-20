e6ad64f9935a70343fda20971c3f4527cfee8366
Alsaev Dmitry <dima101203@gmail.com>
2025-01-20 19:03:54 +0800

    small changes

--- medium/How Much is True/program.js
+++ medium/How Much is True/program.js
@@ -1,5 +1,5 @@
 function countTrue(array){
-    return array.length > 0 ? array.reduce((prev, current) => prev += current,) : 0;
+    return array.reduce((prev, current) => prev += current, 0);
 }
 
 console.log(countTrue([true, false, false, true, false]));
